ca9b278e4c338e0c165fbad926d10e6e0c8ab91d
Hemal Shah <146821698+shahh7@users.noreply.github.com>
2024-11-19 14:56:59 +0000

    changes to pom.xml and mytest.java

--- src/test/java/org/example/MyTest.java
+++ src/test/java/org/example/MyTest.java
@@ -1,16 +1,28 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyTest {
 
     @Test()
     public void myTestMethod() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        driver = new ChromeDriver();
         driver.get("https://www.google.com");
 
         System.out.println("Hello World");
+        Assert.assertTrue(1 == 1, "Test Passed");
+        Assert.assertTrue(1 == 2, "Test Failed");
     }
 }
