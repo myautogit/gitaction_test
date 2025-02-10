8cd2ddda0ac56f2cdf13b03b263ba56ab3b8073a
Hemal Shah <146821698+shahh7@users.noreply.github.com>
2025-02-10 15:42:50 +0000

    added extra files

---
+++ src/main/java/com/qa/utils/AbstractDataLibrary.java
@@ -0,0 +1,12 @@
+package com.qa.utils;
+
+public class AbstractDataLibrary {
+//    public static String takeScreenshotPath = System.getProperty("user.dir")+ "\\screenshot/Test";
+    public static String takeScreenshotPath = "target/screenshots/";
+    public static String testDataPath = "src/test/resources/testData/";
+    public static String configPath = "src/test/resources/config/";
+    public static String driverPath = "src/test/resources/drivers/";
+    public static String reportPath = "target/reports/";
+    public static String downloadPath = "target/downloads/";
+    public static String logPath = "target/logs/";
+}
--- src/test/java/org/example/MyTest.java
+++ src/test/java/org/example/MyTest.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import static com.qa.utils.utilities.takeScreenshot;
 
 public class MyTest {
 
@@ -25,7 +28,7 @@ public class MyTest {
         System.out.println("Test1 - navigate to google.com");
         Assert.assertTrue(1 == 1, "Test Passed");
         System.out.println("Test1 completed - test to pass");
-        Allure.step("This is Allure step - test1 navigate to google.com completed")
+        Allure.step("This is Allure step - test1 navigate to google.com completed");
         driver.quit();
     }
     @Test()
@@ -39,12 +42,14 @@ public class MyTest {
         options.addArguments("--disable-gpu");
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
+        takeScreenshot("test2", driver);
 
         System.out.println("Hello World2");
         SoftAssert softAssert = new SoftAssert();
                 softAssert.assertTrue(1 == 2, "Test Failed");
         System.out.println("Test2 completed - test to fail");
-        Allure.step("This is Allure step - test2 hello world completed")
+        Allure.step("This is Allure step - test2 hello world completed");
         driver.quit();
+        softAssert.assertAll();
     }
 }
