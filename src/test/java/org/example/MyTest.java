4fc39df26c485ad9b44101a65011fbdec35c8140
Hemal Shah <146821698+shahh7@users.noreply.github.com>
2025-02-11 15:25:06 +0000

    trial.yml updated

--- src/test/java/org/example/MyTest.java
+++ src/test/java/org/example/MyTest.java
@@ -2,16 +2,43 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Allure;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 import static com.qa.utils.utilities.takeScreenshot;
 
 public class MyTest {
+
+    private WebDriver driver;
+//    @BeforeClass
+//    public WebDriver wdSetUp() throws MalformedURLException {
+//        ChromeOptions browserOptions = new ChromeOptions();
+//        browserOptions.setPlatformName("Windows 10");
+//        browserOptions.setBrowserVersion("132");
+//        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+//        ltOptions.put("username", "hemal.shahmoodys");
+//        ltOptions.put("accessKey", "oAWsprAnwyHH0kvb1tFhoZdyVeG3RR5FY32dRbSibFH5jjNmWM");
+//        ltOptions.put("build", "selenium build");
+//        ltOptions.put("project", "LambdaTestProj");
+//        ltOptions.put("selenium_version", "4.0.0");
+//        ltOptions.put("w3c", true);
+//        browserOptions.setCapability("LT:Options", ltOptions);
+//        driver = new RemoteWebDriver(new URL("https://hub.lambdatest.com/wd/hub"),browserOptions);
+//        System.out.println("This is before class");
+//        return driver;
+//
+//    }
 
     @Test()
     public void myTestMethod() {
@@ -41,10 +68,18 @@ public class MyTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
+        driver.get("https://www.bbc.co.uk");
         takeScreenshot("test2", driver);
 
-        System.out.println("Hello World2");
+        Assert.assertTrue(driver.getTitle().contains("BBC"), "Test Passed");
+        driver.findElement(By.xpath("//span[text()='Sign in']")).click();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("navigate to bbc");
         SoftAssert softAssert = new SoftAssert();
                 softAssert.assertTrue(1 == 2, "Test Failed");
         System.out.println("Test2 completed - test to fail");
